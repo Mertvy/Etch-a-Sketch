f55f3a3e5080e040aca56fa20da06c6c3b89c0f8
Nicholas Weiss <njweiss4@gmail.com>
2022-06-07 15:50:14 -0400

    modified UI

--- script.js
+++ script.js
@@ -3,38 +3,45 @@ let gridSize = 16;
 grid = document.querySelector("#grid")
 
 let mouseDown = false
-grid.addEventListener('click', 
-() => {
-    if (!mouseDown) {
-        mouseDown = true
-        return
+document.body.onmousedown = () => {mouseDown = true}
+document.body.onmouseup = () => {mouseDown = false}
+function createGrid(gridSize){
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild)
     }
-    mouseDown = false
-    return
-})
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        grid.appendChild(row)
+    }
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div')
-    row.classList.add('row')
-    grid.appendChild(row)
+    rows = document.querySelectorAll(".row")
+
+    let color = "red"
+
+    rows.forEach(row => {
+    for (let i = 0; i < gridSize; i++){
+        const gridSquare = document.createElement('div')
+        gridSquare.classList.add('gridSquare')
+        gridSquare.addEventListener('mouseover', () => {
+            if (!mouseDown) return;
+            gridSquare.style.backgroundColor = color
+            return
+        })
+        gridSquare.addEventListener('mousedown', () => {
+            gridSquare.style.backgroundColor = color
+        })
+        row.appendChild(gridSquare)
+    }    
+    });
 }
 
-rows = document.querySelectorAll(".row")
+createGrid(gridSize)
 
-let color = "red"
-
-rows.forEach(row => {
-for (let i = 0; i < gridSize; i++){
-    const gridSquare = document.createElement('div')
-    gridSquare.classList.add('gridSquare')
-    gridSquare.addEventListener('mouseover', () => {
-        if (!mouseDown) return;
-        gridSquare.style.backgroundColor = color
-        return
-    })
-    gridSquare.addEventListener('click', () => {
-        gridSquare.style.backgroundColor = color; })
-    row.appendChild(gridSquare)
-}    
-});
-
+let slider = document.querySelector(".slider .slider")
+let output = document.querySelector(".output")
+output.innerText = `Grid Dimensions: ${slider.value}x${slider.value}`
+slider.oninput = () => {
+    output.innerText = `Grid Dimensions: ${slider.value}x${slider.value}`
+    createGrid(slider.value)
+}
